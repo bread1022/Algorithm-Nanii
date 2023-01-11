99ec0bcd22959b3c34b7274ff5eefded7fe6b306
NANII <breadnanii@gmail.com>
2023-01-11 14:01:00 +0900

    자료구조 스택과 큐 구현하기

---
+++ Datatype/1_stack.js
@@ -0,0 +1,75 @@
+/**
+ * STACK [스택]
+ *
+ * 동작원리
+ * 1. 삽입, 삭제가 리스트의 한쪽(Top)에서만 이루어진다 (후입선출 : 프링글스 과자)
+ * 2. 0개 이상의 요소를 지닌 선형리스트의 일종
+ * 3. 스택 ADT : init(), create(), is_empty(), is_full(), push(e), pop(), peek()
+ * 4. 스택 연산 : top(), push(), pop(), isEmpty(), isFull()
+ * 예시 : 되돌리기 기능 구현 / 괄호검사 / 계산기 / 미로탐색 등
+ */
+
+const MAX_STACK_SIZE = 10;
+
+class Stack {
+  constructor() {
+    this.stack = [];
+    this.top = -1; //입출력이 이루어지는 곳 => 비어있을 경우(초기상태) = -1
+    this.stackSize = MAX_STACK_SIZE;
+  }
+
+  push(e) {
+    if(!this.isFull()) {
+      this.stack.push(e);
+      this.top++;
+    }
+    else throw Error ('Stack is full');
+  }
+
+  pop() {
+    if(!this.isEmpty()) {
+      this.top--;
+      this.stack.pop();
+    }
+    else throw Error ('Stack is empty');
+  }
+
+  //top 요소 반환 (삭제 X)
+  peek() {
+    return this.stack[this.top];
+  }
+
+  //스택에 element 없으면 true (비어있는지 검사)
+  isEmpty() {
+    return this.stack.length === 0 ? true : false;
+  }
+
+  //스택에 element 있으면 true (가득차있는지 검사)
+  isFull() {
+    return this.stack.length === this.stackSize ? true : false;
+  }
+}
+
+const stack = new Stack();
+
+// ✅ 입력 확인
+for(let i = 1; i <= 10; i++) {
+  stack.push(i);
+}
+console.log('스택 10개의 요소 삽입 : ', stack);
+stack.pop();
+console.log('스택 1개의 요소 삭제 : ', stack);
+console.log('스택 top 반환 : ', stack.peek());
+console.log('스택 empty 확인 : ', stack.isEmpty());
+for(let i = 1; i <= 9; i++) {
+  stack.pop();
+}
+console.log('스택 pop 10번 후 empty 확인 : ', stack.isEmpty());
+// stack.pop(); //else throw Error ('Stack is empty');
+stack.push(1);
+console.log('스택 push 1번 후 full 확인 : ', stack.isFull());
+for(let i = 1; i <= 9; i++) {
+  stack.push(i);
+}
+console.log('스택 push 10번 후 full 확인 : ', stack.isFull());
+// stack.push(1); //else throw Error ('Stack is full')
---
+++ Datatype/2_queue.js
@@ -0,0 +1,82 @@
+/**
+ * QUEUE [큐]
+ *
+ * 동작원리
+ * 1. 삽입은 tail 에서, 삭제는 head 이루어진다 (선입선출 : FIFO = first - in - first - out)
+ * 2. 데이터를 넣을 때마다 요소가 이동 (삽입 O(1), 삭제 O(n), 인덱스 증가만)
+ * 3. 입력된 순서대로 순차적으로 처리
+ * 4. 큐 ADT : head, tail, enqueue, dequeue, isEmpty, size
+ * 5. 큐 연산 : enqueue(), dequeue(), isEmpty(), size()
+ * 예시 : BFS탐색, 컴퓨터 버퍼에서 주로 사용, 순차적으로 처리해야하는 분야에서 활용
+ */
+
+const MAX_QUEUE_SIZE = 10;
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.head = 0;
+    this.tail;
+    this.queueSize = MAX_QUEUE_SIZE;
+  }
+
+  //tail에서 추가
+  enqueue(e) {
+    if(this.size() < this.queueSize) {
+      this.queue.push(e);
+      this.tail = this.queue.length - 1;
+    } else throw Error ('Queue is full');
+  }
+
+  //head에서 삭제
+  dequeue() {
+    if(this.size() > 0) {
+      this.queue.shift();
+      this.tail = this.queue.length - 1;
+    } else throw Error ('Queue is empty');
+  }
+
+  isEmpty() {
+    return this.queue.length === 0 ? true : false;
+  }
+
+  size() {
+    return this.queue.length;
+  }
+
+  //연산이 아닌데 구현..했네..요
+  front() {
+    if(!this.isEmpty()) {
+      return this.queue[this.head];
+    } else throw Error ('Queue not exist');
+  }
+
+  rear() {
+    if(!this.isEmpty()) {
+      return this.queue[this.tail];
+    } else throw Error ('Queue not exist');
+  }
+}
+
+const queue = new Queue();
+
+// ✅ 입력 확인
+for(let i = 1; i <= 10; i++) {
+  queue.enqueue(i);
+}
+console.log('큐 10개의 요소 삽입 : ', queue);
+queue.dequeue();
+console.log('큐 1개의 요소 삭제 : ', queue);
+console.log('큐 head 반환 : ', queue.front());
+console.log('큐 empty 확인 : ', queue.isEmpty()); //false
+for(let i = 1; i <= 9; i++) {
+  queue.dequeue();
+}
+console.log('큐 dequeue 10번 후 empty 확인 : ', queue.isEmpty());
+// queue.dequeue(); //Error: Queue is empty
+for(let i = 1; i <= 9; i++) {
+  queue.enqueue(i);
+}
+queue.enqueue(100);
+console.log(queue);
+// queue.enqueue(200); // Error: Queue is full
